03ec9f5700b14c72efe4efe855e626a26663fd56
BrTsT <bartlomiej.rycek@student.wat.edu.pl>
2020-05-06 13:05:37 +0200

    formularz

---
+++ KWZP/SZWALNIA/SZWALNIA/MaszynyProcesWykonaj.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SZWALNIA
+{
+    partial class MaszynyProcesWykonaj
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIdMaszynyProces = new System.Windows.Forms.Label();
+            this.dgvRodzajMaszyny = new System.Windows.Forms.DataGridView();
+            this.txtLiczbaRbh = new System.Windows.Forms.TextBox();
+            this.txtLiczba = new System.Windows.Forms.TextBox();
+            this.txtRodzajMaszyny = new System.Windows.Forms.TextBox();
+            this.lblLiczbaRbh = new System.Windows.Forms.Label();
+            this.lblLiczba = new System.Windows.Forms.Label();
+            this.lblRodzajMaszyny = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRodzajMaszyny)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblIdMaszynyProces
+            // 
+            this.lblIdMaszynyProces.AutoSize = true;
+            this.lblIdMaszynyProces.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblIdMaszynyProces.Location = new System.Drawing.Point(10, 13);
+            this.lblIdMaszynyProces.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblIdMaszynyProces.Name = "lblIdMaszynyProces";
+            this.lblIdMaszynyProces.Size = new System.Drawing.Size(332, 39);
+            this.lblIdMaszynyProces.TabIndex = 54;
+            this.lblIdMaszynyProces.Text = "ID_Maszyna_Proces";
+            // 
+            // dgvRodzajMaszyny
+            // 
+            this.dgvRodzajMaszyny.AllowUserToAddRows = false;
+            this.dgvRodzajMaszyny.AllowUserToDeleteRows = false;
+            this.dgvRodzajMaszyny.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRodzajMaszyny.Location = new System.Drawing.Point(9, 188);
+            this.dgvRodzajMaszyny.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvRodzajMaszyny.Name = "dgvRodzajMaszyny";
+            this.dgvRodzajMaszyny.ReadOnly = true;
+            this.dgvRodzajMaszyny.RowHeadersWidth = 51;
+            this.dgvRodzajMaszyny.RowTemplate.Height = 24;
+            this.dgvRodzajMaszyny.Size = new System.Drawing.Size(779, 145);
+            this.dgvRodzajMaszyny.TabIndex = 53;
+            // 
+            // txtLiczbaRbh
+            // 
+            this.txtLiczbaRbh.Location = new System.Drawing.Point(127, 158);
+            this.txtLiczbaRbh.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtLiczbaRbh.Name = "txtLiczbaRbh";
+            this.txtLiczbaRbh.Size = new System.Drawing.Size(296, 22);
+            this.txtLiczbaRbh.TabIndex = 52;
+            // 
+            // txtLiczba
+            // 
+            this.txtLiczba.Location = new System.Drawing.Point(127, 125);
+            this.txtLiczba.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtLiczba.Name = "txtLiczba";
+            this.txtLiczba.Size = new System.Drawing.Size(296, 22);
+            this.txtLiczba.TabIndex = 51;
+            // 
+            // txtRodzajMaszyny
+            // 
+            this.txtRodzajMaszyny.Location = new System.Drawing.Point(127, 93);
+            this.txtRodzajMaszyny.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRodzajMaszyny.Name = "txtRodzajMaszyny";
+            this.txtRodzajMaszyny.Size = new System.Drawing.Size(296, 22);
+            this.txtRodzajMaszyny.TabIndex = 50;
+            // 
+            // lblLiczbaRbh
+            // 
+            this.lblLiczbaRbh.AutoSize = true;
+            this.lblLiczbaRbh.Location = new System.Drawing.Point(9, 158);
+            this.lblLiczbaRbh.Name = "lblLiczbaRbh";
+            this.lblLiczbaRbh.Size = new System.Drawing.Size(74, 17);
+            this.lblLiczbaRbh.TabIndex = 49;
+            this.lblLiczbaRbh.Text = "Liczba rbh";
+            // 
+            // lblLiczba
+            // 
+            this.lblLiczba.AutoSize = true;
+            this.lblLiczba.Location = new System.Drawing.Point(9, 125);
+            this.lblLiczba.Name = "lblLiczba";
+            this.lblLiczba.Size = new System.Drawing.Size(49, 17);
+            this.lblLiczba.TabIndex = 48;
+            this.lblLiczba.Text = "Liczba";
+            // 
+            // lblRodzajMaszyny
+            // 
+            this.lblRodzajMaszyny.AutoSize = true;
+            this.lblRodzajMaszyny.Location = new System.Drawing.Point(9, 93);
+            this.lblRodzajMaszyny.Name = "lblRodzajMaszyny";
+            this.lblRodzajMaszyny.Size = new System.Drawing.Size(111, 17);
+            this.lblRodzajMaszyny.TabIndex = 47;
+            this.lblRodzajMaszyny.Text = "Rodzaj Maszyny";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(425, 13);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 36);
+            this.label8.TabIndex = 46;
+            // 
+            // MaszynyProcesWykonaj
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 346);
+            this.Controls.Add(this.lblIdMaszynyProces);
+            this.Controls.Add(this.dgvRodzajMaszyny);
+            this.Controls.Add(this.txtLiczbaRbh);
+            this.Controls.Add(this.txtLiczba);
+            this.Controls.Add(this.txtRodzajMaszyny);
+            this.Controls.Add(this.lblLiczbaRbh);
+            this.Controls.Add(this.lblLiczba);
+            this.Controls.Add(this.lblRodzajMaszyny);
+            this.Controls.Add(this.label8);
+            this.Name = "MaszynyProcesWykonaj";
+            this.Text = "MaszynyProcesWykonaj";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRodzajMaszyny)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIdMaszynyProces;
+        private System.Windows.Forms.DataGridView dgvRodzajMaszyny;
+        private System.Windows.Forms.TextBox txtLiczbaRbh;
+        private System.Windows.Forms.TextBox txtLiczba;
+        private System.Windows.Forms.TextBox txtRodzajMaszyny;
+        private System.Windows.Forms.Label lblLiczbaRbh;
+        private System.Windows.Forms.Label lblLiczba;
+        private System.Windows.Forms.Label lblRodzajMaszyny;
+        private System.Windows.Forms.Label label8;
+    }
+}
---
+++ KWZP/SZWALNIA/SZWALNIA/MaszynyProcesWykonaj.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SZWALNIA
+{
+    public partial class MaszynyProcesWykonaj : Form
+    {
+       public MaszynyProcesWykonaj(SzwalniaEntities db, String maszynyProces)
+        {
+            InitializeComponent();
+            Proces_Zamowienie proces = db.Proces_Zamowienie.Where(id => id.ID_Proces_Technologiczny.ToString() == maszynyProces).First();
+            lblIdMaszynyProces.Text = proces.ID_Proces_Technologiczny.ToString();
+            Maszyny_Proces dane = db.Maszyny_Proces(rodzaj => rodzaj);
+            txtRodzajMaszyny.Text = dane.Rodzaj_Maszyny.ToString();
+            txtLiczba.Text = dane.Liczba_Maszyn.ToString();
+            txtLiczbaRbh.Text = dane.Liczba_Rbh_Maszyna.ToString();
+            dgvRodzajMaszyny.DataSource = db.vMaszyny_rodzaj_liczba.Where(rodzaj => rodzaj.ID_Proces_Technologiczny.ToString() == maszynyProces).ToList();
+        }
+    }
+}
